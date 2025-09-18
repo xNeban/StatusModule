5972c33c5f7fcc95c0bea4f85dddada6bf21d9d4
Gabriel <gabrielkagayutan@hotmail.com>
2025-09-18 12:03:37 +1000

    Update config.php

--- Config/config.php
+++ Config/config.php
@@ -5,6 +5,6 @@ return [
 	'some_parameter' => true,
 
     'options' => [
-        'dashboard_text' => ['default' => __('This text is displayed by SampleModule')],
+        'dashboard_text' => ['default' => __('This text is displayed by SampleModule. I added this text.')],
     ],
 ];
